46908add4f7075684b60809d72364fe72215142f
Younwoo Ki <dewonsunrise@gmail.com>
2024-06-17 21:31:01 +0900

    update

--- configureCitationsAndRelationshipsTable.py
+++ configureCitationsAndRelationshipsTable.py
@@ -6,10 +6,10 @@ from pmidcite.icite.downloader import get_downloader
 import datetime
 from dateutil import parser
 
-server = 'localhost'
-user = 'sa'
-password = 'Password1!'
-db = 'ba'
+server = "localhost"
+user = "ts06067"
+password = "Dream2002!"
+db = "ba"
 dnldr = get_downloader(details_cites_refs="citations")
 
 conn = pymssql.connect(server, user, password, database=db)
@@ -24,23 +24,28 @@ select * from dbo.paper order by citedby_count desc
 cursor.execute(query)
 rows = cursor.fetchall()
 
-insert_papers_query = 'insert into dbo.paper output Inserted.paper_id values (%s, %s, %s, %s, %s, %s, %s)'
-insert_citations_query = 'insert into dbo.citation output Inserted.citing_id values (%s, %s, %s)'
-insert_relationships_query = 'insert into dbo.relationship values (%s, %s, %s)'
-select_citations_query = 'select citing_id from dbo.citation where doi = (%s)'
+insert_papers_query = (
+    "insert into dbo.paper output Inserted.paper_id values (%s, %s, %s, %s, %s, %s, %s)"
+)
+insert_citations_query = (
+    "insert into dbo.citation output Inserted.citing_id values (%s, %s, %s)"
+)
+insert_relationships_query = "insert into dbo.relationship values (%s, %s, %s)"
+select_papers_query = "select paper_id from dbo.paper where doi = (%s)"
+select_citations_query = "select citing_id from dbo.citation where doi = (%s)"
 
-rows = rows[from_idx:to_idx+1]
+rows = rows[from_idx : to_idx + 1]
 
 for row in tqdm(rows):
     try:
-        source_paper_id = int(row['paper_id'])
-        source_pmid = int(row['pmid'])
-        source_doi = str(row['doi'])
+        source_paper_id = int(row["paper_id"])
+        source_pmid = int(row["pmid"])
+        source_doi = str(row["doi"])
 
         if source_pmid == 0:
             continue
 
-        if source_doi == '':
+        if source_doi == "":
             continue
 
         now = datetime.datetime.now()
@@ -48,7 +53,7 @@ for row in tqdm(rows):
         paper = dnldr.get_paper(source_pmid)
         cited_by = paper.cited_by
 
-        print(f'{source_pmid} is cited by {len(cited_by)} papers')
+        print(f"{source_pmid} is cited by {len(cited_by)} papers")
 
         if len(cited_by) > 0:
             # search if source is already registered
@@ -57,24 +62,24 @@ for row in tqdm(rows):
             conn.commit()
 
             if result is not None:
-                source_citing_id = result['citing_id']
+                source_citing_id = result["citing_id"]
             else:
                 # append source to citations table
                 cursor.execute(insert_citations_query, (source_doi, source_pmid, now))
-                source_citing_id = cursor.fetchone()['citing_id']
+                source_citing_id = cursor.fetchone()["citing_id"]
                 conn.commit()
 
             for cite in cited_by:
                 dct = dict(cite.dct)
-                target_doi = str(dct['doi'])
-                target_title = str(dct['title'])
-                target_pub_date = parser.parse(str(dct['year']))
-                target_journal = str(dct['journal'])
-                target_type_description = 'n/a'
-                target_pmid = int(dct['pmid'])
-                target_citedby_count = int(dct['num_cites_all'])
+                target_doi = str(dct["doi"])
+                target_title = str(dct["title"])
+                target_pub_date = parser.parse(str(dct["year"]))
+                target_journal = str(dct["journal"])
+                target_type_description = "n/a"
+                target_pmid = int(dct["pmid"])
+                target_citedby_count = int(dct["num_cites_all"])
 
-                if target_doi == '':
+                if target_doi == "":
                     continue
 
                 # search if target is already registered
@@ -83,24 +88,49 @@ for row in tqdm(rows):
                 conn.commit()
 
                 if result is not None:
-                    target_citing_id = result['citing_id']
+                    target_citing_id = result["citing_id"]
                 else:
                     # append target to citations table
-                    cursor.execute(insert_citations_query, (target_doi, target_pmid, now))
-                    target_citing_id = cursor.fetchone()['citing_id']
+                    cursor.execute(
+                        insert_citations_query, (target_doi, target_pmid, now)
+                    )
+                    target_citing_id = cursor.fetchone()["citing_id"]
                     conn.commit()
 
-                # append target to papers table
-                """
-                cursor.execute(insert_papers_query, (target_doi, target_pmid, target_title, target_pub_date, target_journal, target_type_description, target_citedby_count))
-                target_paper_id = cursor.fetchone()['paper_id']
+                # search if target is already registered
+                cursor.execute(select_papers_query, (target_doi))
+                result = cursor.fetchone()
                 conn.commit()
-                """
+
+                if result is not None:
+                    target_paper_id = result["paper_id"]
+                else:
+                    # append target to papers table
+                    cursor.execute(
+                        insert_papers_query,
+                        (
+                            target_doi,
+                            target_pmid,
+                            target_title,
+                            target_pub_date,
+                            target_journal,
+                            target_type_description,
+                            target_citedby_count,
+                        ),
+                    )
+                    target_paper_id = cursor.fetchone()["paper_id"]
+                    conn.commit()
 
                 # append both mappings to relationships table
-                cursor.execute(insert_relationships_query, (source_citing_id, target_citing_id, 'citing'))
+                cursor.execute(
+                    insert_relationships_query,
+                    (source_citing_id, target_citing_id, "citing"),
+                )
                 conn.commit()
-                cursor.execute(insert_relationships_query, (target_citing_id, source_citing_id, 'cited'))
+                cursor.execute(
+                    insert_relationships_query,
+                    (target_citing_id, source_citing_id, "cited"),
+                )
                 conn.commit()
     except Exception as e:
         print(e)
--- configurePapersTable.py
+++ configurePapersTable.py
@@ -1,15 +1,15 @@
 import pymssql
 import pandas
 
-server = 'localhost'
-user = 'sa'
-password = 'Password1!'
-db = 'ba'
+server = "localhost"
+user = "ts06067"
+password = "Dream2002!"
+db = "ba"
 
 conn = pymssql.connect(server, user, password, database=db)
 cursor = conn.cursor(as_dict=True)
 
-delete_query = 'delete from dbo.paper'
+delete_query = "delete from dbo.paper"
 reseed_query = "dbcc checkident('dbo.paper', reseed, 0)"
 
 cursor.execute(delete_query)
@@ -19,9 +19,26 @@ conn.commit()
 
 for year in range(2008, 2025):
     try:
-        df = pandas.read_csv(f'queryResult-{year}.csv', encoding='utf-8')
-        df = df.rename(columns= {"pubmed_id": "pmid", "coverDate": "pub_date", "publicationName": "journal", "subtype": "type_description"})[['doi', 'pmid', 'title', 'pub_date', 'journal', 'type_description', 'citedby_count']]
-        df = df.fillna('')
+        df = pandas.read_csv(f"queryResult-{year}.csv", encoding="utf-8")
+        df = df.rename(
+            columns={
+                "pubmed_id": "pmid",
+                "coverDate": "pub_date",
+                "publicationName": "journal",
+                "subtype": "type_description",
+            }
+        )[
+            [
+                "doi",
+                "pmid",
+                "title",
+                "pub_date",
+                "journal",
+                "type_description",
+                "citedby_count",
+            ]
+        ]
+        df = df.fillna("")
         query = "INSERT INTO dbo.paper VALUES (%s, %s, %s, %s, %s, %s, %s)"
         sql_data = tuple(map(tuple, df.values))
         cursor.executemany(query, sql_data)
