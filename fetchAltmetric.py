e222050593d76113539d13ec81717af07ab37476
Younwoo Ki <dewonsunrise@gmail.com>
2024-06-30 11:59:45 +0900

    Create fetchAltmetric.py

---
+++ fetchAltmetric.py
@@ -0,0 +1,7 @@
+"""
+https://www.altmetric.com/explorer/api/research_outputs/attention?
+digest=9619d6b7c7f429730c23eefee0d2636b5ae85000
+&filter[doi_prefix]=10.1016
+&filter[title]=Transcatheter,Edge-to-Edge,Repair,for,Treatment,of,Tricuspid,Regurgitation
+&key=99a6796c5e2f4bb48dce7c9059b9e41a
+"""
